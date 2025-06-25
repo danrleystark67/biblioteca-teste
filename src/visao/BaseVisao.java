c57e7d142572a5f97ef17666107d87c9d27eff1b
danrleystark67 <danrleystark67@gmail.com>
2025-06-25 15:25:31 -0400

    Create BaseVisao.java

---
+++ src/visao/BaseVisao.java
@@ -0,0 +1,15 @@
+package visao;
+import java.util.Scanner;
+public abstract class BaseVisao extends BaseUtil {
+    protected abstract void listar();
+    protected abstract void exibir();
+    protected abstract void incluir();
+    protected abstract void alterar();
+    protected abstract void excluir();
+
+public abstract void carregar();
+
+public BaseVisao(Scanner sc){
+    super(sc);
+}
+}
